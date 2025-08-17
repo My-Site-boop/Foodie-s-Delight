494dde0db29f0eed74dca037ad176d452ffd598f
K ROHIT <rohitk20112003@gmail.com>
2025-08-17 21:11:04 +0530

    Update Header.jsx

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,22 +1,58 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link,NavLink } from 'react-router-dom'
 
 
 export default function Header() {
+
+    const [query, setQuery] = useState("");
+
+  // Mapping food names → section ids
+  const foodMap = {
+    pizza: "pizza-section",
+    burger: "burger-section",
+    pasta: "pasta-section",
+    paneer : "paneer -section",
+    dessert: "dessert-section",
+     meal:"main-mail",
+     sushi:"sushi-platter",
+     grilled:"grilled-steak",
+     pancakes:"fluffy-pancakes",
+     omelette:"cheese-omelette",
+  };
+
+  const handleSearch = () => {
+    const val = query.toLowerCase();
+
+    // Loop through keys and check if query includes the food name
+    for (let key in foodMap) {
+      if (val.includes(key)) {
+        const section = document.getElementById(foodMap[key]);
+        if (section) {
+          section.scrollIntoView({ behavior: "smooth" });
+        }
+        return;
+      }
+    }
+
+    alert("No matching section found!");
+  };
+
   return (
     <>
 <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-4 py-3">
-  <NavLink to="/"  className="navbar-brand fs-3 fw-bold text-warning">🍽️ Foodie's Delight</NavLink>
+  <NavLink to="/"  className="navbar-brand fs-3 fw-bold text-warning">🍽️ Foodie's Delight
+  </NavLink>
   <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
     <span className="navbar-toggler-icon"></span>
   </button>
     
    { /* <!-- search bar --> */}
- <div className="col-lg-6 col-sm-8" id="box" ><div id="sc"> <input type="data" placeholder="Search..." id="search-bar" onkeypress="xyz()"/> <button onclick="abc()" id="dox"><span className="material-symbols-outlined" >
+ <div className="col-lg-6 col-sm-8" id="box" ><div id="sc"> <input type="data" placeholder="Search..." id="search-bar" onkeypress="xyz()"  value={query}
+          onChange={(e) => setQuery(e.target.value)}/> <button onclick="abc()" id="dox"><span className="material-symbols-outlined" >
 close
 </span></button></div>
       
-      <button onclick="search()" id="icon1"><span className="material-symbols-outlined" id="icon"> search </span></button>  
+      <button id="icon1" onClick={handleSearch}><span className="material-symbols-outlined" id="icon"> search </span></button>  
 </div>
       
     
